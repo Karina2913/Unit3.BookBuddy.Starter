8b3c68401dda0d4961430bfc58a100861314935c
Karina <karinapliegovergara@gmail.com>
2024-10-14 00:31:20 -0500

    feat: navigations

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,18 @@ import bookLogo from './assets/books.png'
 import { Routes, Route, Link } from 'react-router-dom'
 import Books from './components/Books'
 import SingleBook from './components/SingleBook'
+import Navigations from './components/Navigations'
 
 export default function App() {
   const [token, setToken] = useState(null)
 
   return (
-    <>
+    <div>
+      <Navigations token={token} setToken={setToken} />
       <Routes>
         <Route path="/" element={<Books />} />
         <Route path="/books/:id" element={<SingleBook />} />
       </Routes>
-    </>
+    </div>
   )
 }
--- src/components/Navigations.jsx
+++ src/components/Navigations.jsx
@@ -1,1 +1,26 @@
 /* TODO - add your code to create a functional React component that renders a navigation bar for the different views in your single page application. You may consider conditionally rendering some options - for example 'Login' should be available if someone has not logged in yet. */
+import { Link, useNavigate } from 'react-router-dom';
+
+export default function Navigations ({ token, setToken }) {
+    const navigate = useNavigate("");
+
+    const handleLogOut = () => {
+        setToken(null);
+        navigate("/login");
+    }
+
+    return (
+        <nav className="navbar">
+            <div className="links">
+                {token ? (
+                    <button onClick={handleLogOut}>Log Out</button>
+                ) : (
+                    <>
+                        <Link className="link" to="/register">Sign Up</Link>
+                        <Link className="link" to="/login">Log In</Link>
+                    </>
+                )}
+            </div>
+        </nav>
+    )
+}
