3b3a7f145f9bbb30214e0c3adcca422112e2f500
fguzman717 <felixguzman717@gmail.com>
2024-10-18 20:55:00 -0500

    Book Card Styling

--- src/components/BookAvailable.jsx
+++ src/components/BookAvailable.jsx
@@ -1,26 +1,30 @@
 import { bookAvailability } from "../API";
-import { useState } from 'react';
+import { useState } from "react";
 
-export default function BookAvailable ({ book }) {
-    const [isAvailable, setIsAvailable] = useState(book.available);
-    const token = localStorage.getItem("token");
+export default function BookAvailable({ book }) {
+  const [isAvailable, setIsAvailable] = useState(book.available);
+  const token = localStorage.getItem("token");
 
-    const handleAvailabilityUpdate = async () => {
-        const updatedAvailability = !isAvailable;
-        const updatedBook = await bookAvailability(book.id, updatedAvailability, token);
+  const handleAvailabilityUpdate = async () => {
+    const updatedAvailability = !isAvailable;
+    const updatedBook = await bookAvailability(
+      book.id,
+      updatedAvailability,
+      token
+    );
 
-        if (updatedBook) {
-            setIsAvailable(updatedBook.available);
-        }
+    if (updatedBook) {
+      setIsAvailable(updatedBook.available);
     }
+  };
 
-    console.log("available books", book);
-    return (
-        <div>
-            <h3>This book is: {isAvailable ? 'Available': 'Checked Out'}</h3>
-            <button onClick={handleAvailabilityUpdate}>
-                {isAvailable ? 'Check Out' : 'Return'} Book
-            </button>
-        </div>
-    )
+  console.log("available books", book);
+  return (
+    <div>
+      {/* <h3>This book is: {isAvailable ? 'Available': 'Checked Out'}</h3> */}
+      <button className="check-out-button" onClick={handleAvailabilityUpdate}>
+        {isAvailable ? "Check Out" : "Return"} Book
+      </button>
+    </div>
+  );
 }
--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,11 +1,11 @@
 /* TODO - add your code to create a functional React component that displays all of the available books in the library's catalog. Fetch the book data from the provided API. Users should be able to click on an individual book to navigate to the SingleBook component and view its details. */
-import { useState, useEffect } from 'react';
-import { fetchAllBooks } from '../API';
-import BookCard from './BookCard';
+import { useState, useEffect } from "react";
+import { fetchAllBooks } from "../API";
+import BookCard from "./BookCard";
 
 export default function Books() {
   const [books, setBooks] = useState([]);
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     const getAllBooks = async () => {
@@ -15,34 +15,32 @@ export default function Books() {
       } catch (error) {
         console.error("Whoops! There was an error fetching all books.", error);
       }
-    }
+    };
     getAllBooks();
   }, []);
 
   // using filter to get specific book and storing it in 'filteredBooks'
-  const filteredBooks = books.filter(book => 
+  const filteredBooks = books.filter((book) =>
     book.title.toLowerCase().includes(search.toLowerCase())
   );
 
-    return (
-        <>
-            <div>
-                <input 
-                    type="text"
-                    placeholder="Search book"
-                    value={search}
-                    onChange={(e) => setSearch(e.target.value)}
-                /> 
-            </div>
-            <div>
-                {/* If search has a value, then we're using filteredBooks.map to display 
+  return (
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="Search book"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+      <div className="book-container">
+        {/* If search has a value, then we're using filteredBooks.map to display
                 filtered books. Otherwise, we use books.map and display all books */}
-                {(search ? filteredBooks : books).map((book) => {
-                    return (
-                        <BookCard key={book.id} book={book} setBooks={setBooks} />
-                    );
-                })}
-            </div>
-        </>
-    )
+        {(search ? filteredBooks : books).map((book) => {
+          return <BookCard key={book.id} book={book} setBooks={setBooks} />;
+        })}
+      </div>
+    </>
+  );
 }
